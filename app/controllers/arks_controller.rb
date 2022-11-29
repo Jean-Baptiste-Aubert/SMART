a4d8fd1b1e347faa69407bdb4747c285b8196c43
Justine Di Caro <justinedicaro@gmail.com>
2022-11-29 15:57:02 +0100

    debutcontrollerview

--- app/controllers/arks_controller.rb
+++ app/controllers/arks_controller.rb
@@ -1,7 +1,11 @@
 class ArksController < ApplicationController
+  # représente toute les arks 
+
   def index
+    @arks = Ark.all
   end
 
   def show
+    @ark = Ark.find(params[:id])
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
+  # représente l'image du jour
+  before_action :authenticate_user!
+
   def home
+  end
+
+  def show
+    @ark = Ark.find(params[id])
+    if params[:user].present?
+      redirect_to root_path
+    else
+      @user == current_user
+    end
   end
 end
