c76934c7201dc26b2e37d50e9d74b8e89054a4df
Yoann DESCHAUX <yoanndeschaux@gmail.com>
2022-12-06 17:57:35 +0100

    regex de la mort commentaire ok

--- app/models/ark.rb
+++ app/models/ark.rb
@@ -5,4 +5,8 @@ class Ark < ApplicationRecord
   has_many :users, through: :favorites
   has_many :day_ark
   has_many :day_users, foreign_key: "ark_id", class_name: "DayArk"
+
+  def description_paragraphs
+    description.sub(/.+?(?=[A-Z])/m, "").split(/([.!?])(?<!cf|[A-Z]\.)\s*(?=[A-Z])/).each_slice(6).to_a
+  end
 end
